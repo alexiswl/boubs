3e48a0485ff7e9620364cf4a6a9c633b096f16a4
alexiswl <awlucattini@icloud.com>
2017-02-27 16:45:37 +1100

    updated usage script for tiam5.sh

--- tiam5.sh
+++ tiam5.sh
@@ -17,6 +17,12 @@ function display_usage {
 	echo "Usage: tiam5 [-h | --help] <files>"	
 }
 
+help_message='Optional arguments:
+	      -h | --help:    Display this message
+              
+	      Positional arguments:
+	      <files>:        List of tar files to check integrity and md5sum'
+
 # Check there exists at least one argument
 [ $# -eq 0 ] && { display_usage; exit 1; }
 
@@ -25,15 +31,10 @@ for tar_file in "$@"
 do
 	if [ ${tar_file} == "-h" ] || [ ${tar_file} == "--help" ]; then  # help option
 		display_usage;
-		echo ""  # Print an empty line
-		echo "Optional arguments:"
-		echo "-h || --help:    Display this message"
-		echo ""
-		echo "Positional arguments"
-		echo "<files>          list of tar files to check integrity and md5sum"
-		exit 0;
+		echo "${help_message}"
+		exit 0
 	elif [ ! -e tar_file ]; then  # If the file provided is not a file 
-		tostderr "Error ${tar_file} is not a file."
+		tostderr "Error: ${tar_file} is not a file."
 		exit 1  # Now exit the script
 	elif ! tar -tf ${tar_file} &> /dev/null; then
 		tostderr "${tar_file}"
@@ -41,4 +42,4 @@ do
 		md5sum ${tar_file}
 	fi
 done
-
+exit 0  # Script exited sucessfully. 
