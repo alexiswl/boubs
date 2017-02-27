0e44ec6568c6169181925309e17d200fab0f6200
alexiswl <awlucattini@icloud.com>
2017-02-27 16:29:46 +1100

    added usage and help documentation to tiam5.sh

--- tiam5.sh
+++ tiam5.sh
@@ -1,15 +1,44 @@
 #!/usr/bin/env bash
 
-# This script takes in one tar file as an argument
+# Author: Alexis Lucattini - https://www.github.com/alexiswl
+# This script takes in tar files as an argument
 # checks to ensure that it extracts properly (not corrupted)
 # then generates an md5sum of the tar (if it isn't corrupted)
-# Author: Alexis Lucattini - https://www.github.com/alexiswl
 
-tar_file=$1
+# List of functions needed
 
-if ! tar -tf ${tar_file} &> /dev/null; then
-	echo "tar file corrupted"
-else 
-	md5sum ${tar_file}
-fi
+# Print to stderr instead of stdout
+function tostderr {
+	echo "$@" 1>&2; 
+}
+
+# Display usage
+function display_usage {
+	echo "Usage: tiam5 [-h | --help] <files>"	
+}
+
+# Check there exists at least one argument
+[ $# -eq 0 ] && { display_usage; exit 1; }
+
+# Iterate through command line arguments (files and directories)
+for tar_file in "$@"
+do
+	if [ ${tar_file} == "-h" ] || [ ${tar_file} == "--help" ]; then  # help option
+		display_usage;
+		echo ""  # Print an empty line
+		echo "Optional arguments:"
+		echo "-h || --help:    Display this message"
+		echo ""
+		echo "Positional arguments"
+		echo "<files>          list of tar files to check integrity and md5sum"
+		exit 0;
+	elif [ ! -e tar_file ]; then  # If the file provided is not a file 
+		tostderr "Error ${tar_file} is not a file."
+		exit 1  # Now exit the script
+	elif ! tar -tf ${tar_file} &> /dev/null; then
+		tostderr "${tar_file}"
+	else 
+		md5sum ${tar_file}
+	fi
+done
 
